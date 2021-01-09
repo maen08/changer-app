2de00cb21d1ed44530a101a571bf0cbee85b5a54
Stanley <2001stany@gmail.com>
2021-01-10 01:49:01 +0300

    #Amount model created

--- change_app/models.py
+++ change_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+
+AMOUNT_CHOICE = (
+    '5000', '',
+    '2000', '',
+    '1000', '',
+    '500', '',
+    '200', '',
+    '100', '',
+)
+
+
+class Amount(models.Model):
+    total_amount = models.IntegerField()
+    change = models.IntegerField(choices=AMOUNT_CHOICE)
+    multiplier = models.IntegerField()
+
+    def __str__(self):
+        return self.total_amount
+    
