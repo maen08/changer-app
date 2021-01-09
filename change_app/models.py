7cc803dcf6ce56d8edce3792b4e4c5684bdffebf
Stanley <2001stany@gmail.com>
2021-01-10 01:58:31 +0300

    models attribute added to Amount

--- change_app/models.py
+++ change_app/models.py
@@ -2,19 +2,9 @@ from django.db import models
 
 
 
-AMOUNT_CHOICE = (
-    '5000', '',
-    '2000', '',
-    '1000', '',
-    '500', '',
-    '200', '',
-    '100', '',
-)
-
-
 class Amount(models.Model):
     total_amount = models.IntegerField()
-    change = models.IntegerField(choices=AMOUNT_CHOICE)
+    change = models.IntegerField()
     multiplier = models.IntegerField()
 
     def __str__(self):
