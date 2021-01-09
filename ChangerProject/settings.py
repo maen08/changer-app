53382348cc63ea9c06ca22ed69d3d056bf791e9f
Stanley <2001stany@gmail.com>
2021-01-10 02:15:14 +0300

    parsing Amount on frontend

--- ChangerProject/settings.py
+++ ChangerProject/settings.py
@@ -1,5 +1,5 @@
 
-
+import  os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'ChangerProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- change_app/views.py
+++ change_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Amount
 
-# Create your views here.
+
+
+def request_change(request):
+    whole_amount = Amount.objects.all()
+    args = {
+        'whole_amount':whole_amount,
+    }
+    return render(request, template_name='index.html', context=args)
