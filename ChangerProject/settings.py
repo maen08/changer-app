e6f74e579edbcd3846fd338304f1f2989a0665c4
Ramadhan Marijani <ramadhanwakil19@gmail.com>
2021-01-10 03:54:53 +0300

    Merge branch 'master' of https://github.com/maen08/changer-app into rama

--- ChangerProject/settings.py
+++ ChangerProject/settings.py
@@ -39,8 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms'
 
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+AUTH_USER_MODEL = 'user.ChangeUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
